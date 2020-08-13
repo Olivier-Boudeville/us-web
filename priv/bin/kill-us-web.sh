607b6a2bfeed41e320fae80e927396181b10ea27
Olivier Boudeville <olivier.boudeville@esperide.com>
2020-08-13 21:38:29 +0200

    Robustified.

--- priv/bin/kill-us-web.sh
+++ priv/bin/kill-us-web.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo " Killing (not stopping) any US-Web instance found..."
+echo " Killing (not stopping) any US-Web instance found, and EPMD as well..."
 
 kill $(ps -edf | grep us_web | grep -v run_erl | grep -v grep | awk '{ print $2 }') 2>/dev/null
+killall epmd
+
+sleep 1
 
 echo "Resulting US-Web found: $(ps -edf | grep us_web | grep -v grep)"

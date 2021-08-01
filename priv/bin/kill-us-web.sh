c3473eb96eda353b734d0a50df4bba2bc7811b56
Olivier Boudeville <olivier.boudeville@esperide.com>
2021-08-01 11:44:54 +0200

    Kill clarified.

--- priv/bin/kill-us-web.sh
+++ priv/bin/kill-us-web.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-echo " Killing (not stopping) any US-Web instance found, and EPMD as well..."
+# Of course using stop-us-web-{native-build,release}.sh shall be preferred:
+echo " Killing brutally (not stopping gracefully) any US-Web instance found, and EPMD as well..."
 
 kill $(ps -edf | grep us_web | grep -v run_erl | grep -v grep | awk '{ print $2 }') 2>/dev/null
 killall epmd

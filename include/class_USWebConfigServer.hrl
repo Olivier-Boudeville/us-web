d8c40ff9c3e591bfad5a0ff8d3a5d7d0c20694a7
Olivier Boudeville <olivier.boudeville@esperide.com>
2022-03-19 15:21:46 +0100

    Restyled.

--- include/class_USWebConfigServer.hrl
+++ include/class_USWebConfigServer.hrl
@@ -23,38 +23,36 @@
 % General web settings, typically useful for the us_web supervisor:
 -record( general_web_settings, {
 
-		   % Rules so that http accesses are managed by the right handlers:
-		   http_dispatch_rules :: class_USWebConfigServer:dispatch_rules(),
+	% Rules so that http accesses are managed by the right handlers:
+	http_dispatch_rules :: class_USWebConfigServer:dispatch_rules(),
 
-		   % TCP port (ex: 80) to serve any http content:
-		   http_tcp_port :: maybe( net_utils:tcp_port() ),
-
-
-		   % Rules so that https accesses are managed by the right handlers:
-		   https_dispatch_rules :: class_USWebConfigServer:dispatch_rules(),
-
-		   % TCP port (ex: 443) to serve any https content:
-		   https_tcp_port :: maybe( net_utils:tcp_port() ),
+	% TCP port (ex: 80) to serve any http content:
+	http_tcp_port :: maybe( net_utils:tcp_port() ),
 
 
-		   % Tells whether certificates shall be used, and how:
-		   certificate_support :: class_USWebConfigServer:cert_support(),
+	% Rules so that https accesses are managed by the right handlers:
+	https_dispatch_rules :: class_USWebConfigServer:dispatch_rules(),
 
-		   % Including for SNI information:
-		   https_transport_info ::
-				maybe( class_USWebConfigServer:https_transport_info() ),
+	% TCP port (ex: 443) to serve any https content:
+	https_tcp_port :: maybe( net_utils:tcp_port() ),
 
-		   % The absolute path to the Diffie-Helman key file (if any) to ensure
-		   % a secure key exchange with Forward Secrecy:
-		   %
-		   dh_key_path :: maybe( file_utils:bin_file_path() ),
 
-		   % The absolute path to the certificate authority PEM file (if any)
-		   % for the chain of trust:
-		   %
-		   ca_cert_key_path :: maybe( file_utils:bin_file_path() )
+	% Tells whether certificates shall be used, and how:
+	certificate_support :: class_USWebConfigServer:cert_support(),
 
- } ).
+	% Including for SNI information:
+	https_transport_info ::
+			maybe( class_USWebConfigServer:https_transport_info() ),
+
+	% The absolute path to the Diffie-Helman key file (if any) to ensure a
+	% secure key exchange with Forward Secrecy:
+	%
+	dh_key_path :: maybe( file_utils:bin_file_path() ),
+
+	% The absolute path to the certificate authority PEM file (if any) for the
+	% chain of trust:
+	%
+	ca_cert_key_path :: maybe( file_utils:bin_file_path() ) } ).
 
 
 
@@ -90,36 +88,34 @@
 % Information about the analysis of web access logs:
 -record( web_analysis_info, {
 
-		   % Ex: 'awstats'.
-		   tool :: class_USWebConfigServer:log_analysis_tool_name(),
+	% Ex: 'awstats'.
+	tool :: class_USWebConfigServer:log_analysis_tool_name(),
 
 
-		   % Full path to the main analyzer of access logs, to update the
-		   % database thereof:
-		   %
-		   % (typically awstats.pl, possibly in
-		   % /usr/share/webapps/awstats/cgi-bin/)
-		   %
-		   update_tool_path :: maybe( file_utils:bin_executable_path() ),
+	% Full path to the main analyzer of access logs, to update the database
+	% thereof:
+	%
+	% (typically awstats.pl, possibly in /usr/share/webapps/awstats/cgi-bin/)
+	%
+	update_tool_path :: maybe( file_utils:bin_executable_path() ),
 
 
-		   % Full path to the executable in charge of the generation of HTML
-		   % reports:
-		   %
-		   % (typically awstats_buildstaticpages.pl, possibly in
-		   % /usr/share/awstats/tools)
-		   %
-		   report_tool_path :: file_utils:bin_executable_path(),
+	% Full path to the executable in charge of the generation of HTML reports:
+	%
+	% (typically awstats_buildstaticpages.pl, possibly in
+	% /usr/share/awstats/tools)
+	%
+	report_tool_path :: file_utils:bin_executable_path(),
 
 
-		   % As read from template configuration file:
-		   template_content :: binary(),
+	% As read from template configuration file:
+	template_content :: binary(),
 
-		   % Where the configuration file shall be generated:
-		   conf_dir :: file_utils:bin_directory_path(),
+	% Where the configuration file shall be generated:
+	conf_dir :: file_utils:bin_directory_path(),
 
-		   % Where the state files of the log analyzer will be stored:
-		   state_dir :: file_utils:bin_directory_path(),
+	% Where the state files of the log analyzer will be stored:
+	state_dir :: file_utils:bin_directory_path(),
 
-		   % Where the HTML reports will be generated:
-		   web_content_dir :: file_utils:bin_directory_path() } ).
+	% Where the HTML reports will be generated:
+	web_content_dir :: file_utils:bin_directory_path() } ).

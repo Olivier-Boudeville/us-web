497b9e947a5f12775a1b96f4e6d17f110b3dc551
Olivier Boudeville <olivier.boudeville@esperide.com>
2024-12-29 16:35:29 +0100

    Copyright notices updated for 2025.

--- test/us_web_otp_application_test.erl
+++ test/us_web_otp_application_test.erl
@@ -1,4 +1,4 @@
-% Copyright (C) 2021-2024 Olivier Boudeville
+% Copyright (C) 2021-2025 Olivier Boudeville
 %
 % This file belongs to the US-Web project, a part of the Universal Server
 % framework.

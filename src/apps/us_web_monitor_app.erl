bbc149f4672332a2c42916638182a65c98832af3
Olivier Boudeville <olivier.boudeville@esperide.com>
2025-09-30 23:14:51 +0200

    Centralised thanks to us_client.

--- src/apps/us_web_monitor_app.erl
+++ src/apps/us_web_monitor_app.erl
@@ -24,7 +24,7 @@
 -moduledoc """
 Actual US-Web **trace monitoring logic**, as a (Myriad) application.
 
-Typically called through the us_web/priv/bin/monitor-us-web.sh script.
+Typically called through the `us_web/priv/bin/monitor-us-web.sh` script.
 
 Designed to monitor a US-Web instance typically from any remote host able to
 connect to the VM hosting that instance.
@@ -51,62 +51,13 @@ exec() ->
 	%
 	erlang:process_flag( trap_exit, false ),
 
-	% First, enable all possible helper code (hence to be done first of all):
-	update_code_path_for_myriad_from_module(),
+	{ ActualTargetNodeName, IsVerbose, CfgTable, FinalArgTable } =
+        us_client:setup( _ServerPrefix=us_web ),
 
-	% Allows to support both OTP conventions and ad hoc, automatic ones:
-	wooper_utils:start_for_app(),
+	list_table:is_empty( FinalArgTable ) orelse
+		throw( { unexpected_arguments,
+				 list_table:enumerate( FinalArgTable ) } ),
 
-	CfgFilePath = init_from_command_line(),
-
-	Cfg = file_utils:read_etf_file( CfgFilePath ),
-
-	%trace_utils:debug_fmt( "Read configuration from '~ts': ~p",
-	%                       [ CfgFilePath, Cfg ] ),
-
-	[ MainTargetNodeName, UserTargetNodeName ] = get_target_node_names( Cfg ),
-
-	app_facilities:display( "Trying to connect to US-Web node '~ts'.",
-							[ MainTargetNodeName ] ),
-
-	ActualTargetNodeName = case net_adm:ping( MainTargetNodeName ) of
-
-		pong ->
-			MainTargetNodeName;
-
-		pang ->
-			trace_utils:warning_fmt( "Unable to connect to a target main "
-				"node '~ts'; trying an alternate one, based on "
-				"user name: '~ts' (in case of ad hoc launch).",
-				[ MainTargetNodeName, UserTargetNodeName ] ),
-
-			case net_adm:ping( UserTargetNodeName ) of
-
-				pong ->
-					UserTargetNodeName;
-
-				pang ->
-					trace_utils:error_fmt( "Unable to connect to either node "
-						"names, the main one ('~ts') or the user one ('~ts')."
-						"~nIf the target node is really running and is named "
-						"like either of the two, check that the cookies match "
-						"and, finally, that no firewall is in the way "
-						"(e.g. a server may filter the EPMD port of interest).",
-						[ MainTargetNodeName, UserTargetNodeName ] ),
-
-					throw( { unable_to_connect_to,
-							 { MainTargetNodeName, UserTargetNodeName } } )
-
-				end
-
-		end,
-
-
-	% Otherwise the remote node could not be known before use:
-	global:sync(),
-
-	%app_facilities:display( "Globally registered names: ~w.",
-	%                        [ global:registered_names() ] ),
 
 	AggregatorName = ?trace_aggregator_name,
 
@@ -120,9 +71,10 @@ exec() ->
 	AggregatorPid = naming_utils:get_locally_registered_pid_for(
 		AggregatorName, ActualTargetNodeName ),
 
-	app_facilities:display( "Creating now a local trace listener." ),
+	IsVerbose andalso
+        app_facilities:display( "Creating now a local trace listener." ),
 
-	TraceListenerPid = case get_tcp_port_range( Cfg ) of
+	TraceListenerPid = case us_client:get_tcp_port_range( CfgTable ) of
 
 		undefined ->
 			class_TraceListener:synchronous_new_link( AggregatorPid,
@@ -134,12 +86,13 @@ exec() ->
 
 	end,
 
-	app_facilities:display( "Waiting for the trace listener to be closed." ),
+	IsVerbose andalso app_facilities:display(
+                        "Waiting for the trace listener to be closed." ),
 
 	receive
 
 		{ trace_listening_finished, TraceListenerPid } ->
-			app_facilities:display( "Trace listener closed." )
+			IsVerbose andalso app_facilities:display( "Trace listener closed." )
 
 	end,
 
@@ -150,121 +103,3 @@ exec() ->
 	% no aggregator was started from that test.
 	%
 	app_facilities:finished().
-
-
-
--doc "Initialises this application from the command line.".
-init_from_command_line() ->
-
-	% To force options for testing:
-	%ArgTable = cmd_line_utils:generate_argument_table( "--help" ),
-
-	ArgTable = cmd_line_utils:get_argument_table(),
-
-	%trace_utils:debug_fmt( "Argument table: ~ts",
-	%                       [ list_table:to_string( ArgTable ) ] ),
-
-	% Argument expected to be set by the caller script:
-	{ CfgFilePath, ConfigShrunkTable } =
-			case list_table:extract_entry_if_existing( '-config-file',
-													   ArgTable ) of
-
-		false ->
-			throw( no_configuration_file_set );
-
-		{ [ [ CfgPath ] ], CfgShrunkTable } ->
-			case file_utils:is_existing_file_or_link( CfgPath ) of
-
-				true ->
-					{ CfgPath, CfgShrunkTable };
-
-				false ->
-					throw( { configuration_file_not_found, CfgPath } )
-
-			end;
-
-		{ OtherCfgArg, _CfgTable } ->
-			throw( { unexpected_configuration_argument, OtherCfgArg } )
-
-
-	end,
-
-	%trace_utils:debug_fmt( "Configuration file: '~ts'.", [ CfgFilePath ] ),
-
-	% Argument also expected to be set by the caller script:
-	{ RemoteCookie, CookieShrunkTable } =
-			case list_table:extract_entry_if_existing( '-target-cookie',
-													   ConfigShrunkTable ) of
-
-		false ->
-			throw( no_target_cookie_set );
-
-		{ [ [ Cookie ] ], CookShrunkTable } ->
-			{ text_utils:string_to_atom( Cookie ), CookShrunkTable };
-
-		{ OtherCookieArg, _CookTable } ->
-			throw( { unexpected_cookie_argument, OtherCookieArg } )
-
-	end,
-
-	trace_utils:debug_fmt( "Setting remote cookie: '~ts'.", [ RemoteCookie ] ),
-
-	net_utils:set_cookie( RemoteCookie ),
-
-	%trace_utils:debug_fmt( "Remaining arguments: ~ts",
-	%   [ cmd_line_utils:argument_table_to_string( CookieShrunkTable ) ] ),
-
-	list_table:is_empty( CookieShrunkTable ) orelse
-		throw( { unexpected_arguments,
-				 list_table:enumerate( CookieShrunkTable ) } ),
-
-	CfgFilePath.
-
-
-
--doc """
-Returns the possible node names (main or user-based one) corresponding to the
-target server US-Web instance.
-
-Two names are considered, as two approaches can be used to launch US-Web nodes.
-""".
-get_target_node_names( Cfg ) ->
-
-	RemoteHostname = list_table:get_value( us_web_hostname, Cfg ),
-
-	%trace_utils:debug_fmt( "Remote host: '~ts'.", [ RemoteHostname ] ),
-
-	net_utils:localnode() =/= local_node orelse
-		throw( { node_not_networked, node() } ),
-
-	% Supposing here uniform client/server conventions in terms of short or long
-	% names:
-	%
-	NodeNamingMode = net_utils:get_node_naming_mode(),
-
-	% Note that two hardcoded node names are used here, the main one (when run
-	% as a service) and one embedding the name of the current user (when run as
-	% an app, typically for testing; we used to suppose that the user names -
-	% local and server - matched, but now we expect a conventional user to be
-	% used on the server):
-	%
-
-	BaseNodeNames = [ "us_web", text_utils:format( "us_web_exec-~ts",
-									%[ system_utils:get_user_name() ] ) ],
-									[ "web-srv" ] ) ],
-
-	% Returns relevant, ordered candidates:
-	[ net_utils:get_complete_node_name( N, RemoteHostname, NodeNamingMode )
-		|| N <- BaseNodeNames ].
-
-
-
--doc "Returns the TCP port range to use (if any).".
-get_tcp_port_range( Cfg ) ->
-
-	MaybePortRange = list_table:get_value_with_default( _K=tcp_port_range,
-		_Default=undefined, Cfg ),
-
-	%trace_utils:debug_fmt( "TCP port range: ~p.", [ MaybePortRange ] ),
-
-	MaybePortRange.
